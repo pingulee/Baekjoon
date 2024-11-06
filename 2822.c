919b233292064842df5acb88c83f3ae606c6ecc4
pingulee <pigulee@soosan.co.kr>
2024-11-06 21:41:53 +0900

    2822: 스코어 출력만 하면 됨

--- 2822.c
+++ 2822.c
@@ -5,10 +5,16 @@ const int MAX = 8;
 
 int sum_score(int *score) {
     int sum = 0;
-    for (int i = MAX - 1; i >= 3; i--) {
+    for (int i = 3; i < MAX; i++) {
         sum += score[i];
     }
     return sum;
+}
+
+void print_index(int *index) {
+    for (int i = 3; i < MAX; i++) {
+        printf("%d ", index[i]);
+    }
 }
 
 int main(void) {
@@ -26,14 +32,19 @@ int main(void) {
     for (int i = 0; i < MAX - 1; i++) {
         for (int j = i + 1; j < MAX; j++) {
             if (score[i] > score[j]) {
-                int temp = score[i];
+                int temp_score = score[i];
                 score[i] = score[j];
-                score[j] = temp;
+                score[j] = temp_score;
+
+                int temp_index = index[i];
+                index[i] = index[j];
+                index[j] = temp_index;
             }
         }
     }
 
     printf("%d\n", sum_score(score));
+    print_index(index);
 
     free(score);
     free(index);
